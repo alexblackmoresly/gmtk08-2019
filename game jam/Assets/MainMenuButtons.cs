fced9e3b4b86160f20830beda42fdc10d5d6506a
alexblackmoresly <alexblackmoresly@users.noreply.github.com>
2019-08-02 22:15:04 +0100

    yes
    yes

---
+++ game jam/Assets/MainMenuButtons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public int levelChosen;
+    // Start is called before the first frame update
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    void Start()
+    {
+        
+        chooseLevel();
+    }
+    private void chooseLevel()
+    {
+        SceneManager.LoadScene(levelChosen);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ game jam/Assets/movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Vector2 mouseLocation;
+    public LineRenderer lr;
+    public Rigidbody2D rb;
+    public float maxForce;
+    public float maxDistance;
+    private Vector2 direction;
+    private float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        lr = GetComponent<LineRenderer>();
+        lr.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, mouseLocation);
+
+        direction = mouseLocation - (Vector2)transform.position;
+        power = direction.magnitude / maxDistance;
+        power *= maxForce;
+        direction = direction.normalized;
+
+        if (Input.GetMouseButton(0)) {
+            lr.enabled = true;
+        } else {
+            lr.enabled = false;
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            fire();
+        }
+    }
+
+    public void fire() {
+        rb.AddForce(direction * power);
+    }
+}
